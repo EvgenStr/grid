5f7689d5d370c1dc04ea31f90ecf4449a6ecb9d4
EvgenStr <juve2910@gmail.com>
2021-03-18 13:29:14 +0200

    formating files

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,5 @@
-function toogleNavMenu(x) {
+function tooggleNavMenu(x) {
   x.classList.toggle("change");
   let navMenu = document.querySelector(".nav-menu");
   navMenu.classList.toggle("active")
-
 }
